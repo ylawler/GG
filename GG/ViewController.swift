49b4b7b3a4a219ce0791b6366753ab1a03efa9f7
ylawler <lawleryannick1@gmail.com>
2021-03-13 19:49:48 +0100

    Initial Commit

---
+++ GG/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GG
+//
+//  Created by Yannick Lawler on 13.03.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
